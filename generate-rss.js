a5bae3d7e57db42d6362e6cc425983a7f9e36516
aditya <adityajha1104@gmail.com>
2026-03-16 11:40:50 +0530

    Update generate-rss.js

--- generate-rss.js
+++ generate-rss.js
@@ -1,10 +1,10 @@
-const fs = require("fs");
+async function generateRSS() {
+  const siteUrl = "https://aditya-cmd-max.github.io/marina";
 
-const siteUrl = "https://aditya-cmd-max.github.io/marina";
+  const res = await fetch("/marina/articlesmarina.json");
+  const articles = await res.json();
 
-const articles = JSON.parse(fs.readFileSync("articlesmarina.json"));
-
-let rss = `<?xml version="1.0" encoding="UTF-8" ?>
+  let rss = `<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0">
 <channel>
 <title>Reverbit Marina</title>
@@ -13,23 +13,24 @@ let rss = `<?xml version="1.0" encoding="UTF-8" ?>
 <language>en-us</language>
 `;
 
-articles.forEach(article => {
-  rss += `
-  <item>
-  <title>${article.title}</title>
-  <link>${siteUrl}/articles/${article.slug}.html</link>
-  <description>${article.description}</description>
-  <pubDate>${new Date(article.date).toUTCString()}</pubDate>
-  <guid>${siteUrl}/articles/${article.slug}.html</guid>
-  </item>
-  `;
-});
+  articles.forEach(article => {
+    rss += `
+<item>
+<title>${article.title}</title>
+<link>${article.link}</link>
+<description>${article.description}</description>
+<pubDate>${new Date(article.date).toUTCString()}</pubDate>
+<guid>${article.link}</guid>
+</item>
+`;
+  });
 
-rss += `
+  rss += `
 </channel>
 </rss>
 `;
 
-fs.writeFileSync("rss.xml", rss);
+  document.body.textContent = rss;
+}
 
-console.log("RSS feed generated!");
+generateRSS();
