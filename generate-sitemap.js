48f0923155f934dd4896300de7d306052e522da3
aditya <adityajha1104@gmail.com>
2026-03-22 12:36:27 +0530

    Update generate-sitemap.js

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const articles = require('https://marina.reverbit.in/articlesmarina.json');
+const articles = require('./articlesmarina.json');
 
 let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
